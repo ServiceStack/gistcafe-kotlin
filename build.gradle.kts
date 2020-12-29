ab385a5bdd5cf47e2a45ca17d6bc93645fa03ef3
Demis Bellot <demis.bellot@gmail.com>
2020-12-30 02:22:38 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ bintray {
     pkg.apply {
         repo = "maven"
         name =  artifactId
-        userOrg = orgName
+        userOrg = "servicestack"
         websiteUrl = "https://gist.cafe"
         githubRepo = repoName
         vcsUrl = "https://github.com/$repoName"
